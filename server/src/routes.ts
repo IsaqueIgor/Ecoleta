3abfc8bd3dcf85665c4ba015987744fd37665cb7
Isaque Igor da Silva <isaque.silva@industriall.ai>
2020-06-02 23:09:21 -0300

    feat: Creating Controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,20 +1,16 @@
 import express from 'express';
 
-import knex from './database/connection';
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
 
 const routes = express.Router();
 
-routes.get('/items', async(request, response) => {
-    const items = await knex('items').select('*');
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-    const serializedItems = items.map(item => {
-        return{
-            title: item.title,
-            image_url: `http://localhost:3333/uploads/${item.image}`,
-        };
-    });
 
-    return response.json(serializedItems);
-});
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
 
 export default routes;
